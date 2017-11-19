77bc9ec4eb2cc51e67ab50ecb59aa61ca3136f22
Ubuntu <ubuntu@ip-172-31-39-132.us-west-2.compute.internal>
2017-11-19 21:00:14 +0000

    First trial run

--- main.py
+++ main.py
@@ -126,8 +126,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                                feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate: 0.001})
             print("Loss: = {:.3f}".format(loss))
             
-        if (epoch + 1) % 30 == 0: # Save every 20 epochs
-            save_path = saver.save(sess, os.path.join(data_dir, 'cont_epoch_' + str(epoch) + '.ckpt'))
+       # if (epoch + 1) % 30 == 0: # Save every 20 epochs
+       #     save_path = saver.save(sess, os.path.join(data_dir, 'cont_epoch_' + str(epoch) + '.ckpt'))
         
 tests.test_train_nn(train_nn)
 
